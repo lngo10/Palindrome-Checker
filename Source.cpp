0a96b9d0f130d12b9cc81e3a445fb48d75649565
Loc Ngo <locqngo@gmail.com>
2020-02-23 14:26:44 -0600

    Fully Functional

--- Source.cpp
+++ Source.cpp
@@ -1,59 +1,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;	/* palindrome checker
+using namespace std;	
 
-General algorithm:
-1. read string or number
-2. how many characters
-3. even and odd function
-		odd function																			even function
-	 a. find mid char										a. find mid.1 and mid.2 chars
-	 b. does mid+1 and mid-1 exist,							b. dones mid.1 -1 and mid.2+1 esist?
-	 c. if yes, compare mid+1 to mid-1						c. if yes, compare mid.1 -1 and mid.2+1
-			if = repeat [b] and +1 to mid value					if =
-			if != exit loop output "no"
-	 d. if no, output "yes"									d. if no, output "yes"
-
-	 */
-
-//1. read string
-
-int main(){
-string input;
-char rr = 'y';
-while (rr == 'Y' || rr == 'y') {
-	cout << "enter word" << endl;
-	cin >> input;
-	int inputsize = size(input);
-	vector<char> inputword(input.begin(), input.end());
-	for (int i = 0; i < inputsize; i++) {
-		inputword[i] = input[i];
-		cout << inputword[i] << endl;	//test if inputword vector is equal to input
+int main()
+{
+	string input;
+	char rr = 'y';
+	int leave = 0;
+	while (rr == 'Y' || rr == 'y') {
+		cout << "enter word" << endl; cin >> input;
+		int inputsize = size(input);
+		vector<char> inputword(input.begin(), input.end());
+			for(int i = 0; i <= inputsize/2; i++){
+				if (inputword[i] != inputword[inputsize - 1-i]){													
+					leave = 1;
+					break;
+				}														
+			}
+			if (leave) {
+				cout << input << " is not a palindrome" << endl; leave = 0;
+			}
+			else {
+				cout << input << " is a palindrome" << endl;
+			}
+			cout << "rerun program? [Y/N]" << endl; //ask to rerun
+			cin >> rr;
+			cout << endl;
 	}
-	if (inputsize % 2 == 0) { //even function
-		cout << "test for even" << endl; // remove later
-		if (inputword.front() == inputword.back())
-		{
-			cout << "front and back are equal" << endl;
-			/*if (inputword.front( + 1) == inputword.back( - 1) {
-				cout << "front + 1 and back - 1 are equal" << endl;
-			}*/
-		}
-	}
-	/*else if (inputsize % 2 != 0) { // odd function
-		cout << "test for odd" << endl; // remove later
-
-		// test function for odd
-		int oddsize = size(input);
-		vector<char> oddword;
-		for (int oddi = 0; oddi < oddsize; oddi++) { //do something x times depending on size off odd word
-			//oddword.at(oddi) = input.at(oddi);
-			//oddword.
-		}
-	}*/
-
-	cout << "rerun program? [Y/N]" << endl; //ask to rerun
-	cin >> rr;
-	cout << endl;}
 }
